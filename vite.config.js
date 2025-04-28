454c9b5c73f463c045a37fb8844d7618bd682d6e
endercass <46408156+Endercass@users.noreply.github.com>
2025-04-28 18:21:29 -0400

    Fix publish

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
+import dtsPlugin from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [
+    dtsPlugin({
+      insertTypesEntry: true,
+      outputDir: "dist/types",
+      exclude: ["**/*.test.ts"],
+      tsConfigFilePath: "./tsconfig.json",
+    }),
+  ],
   build: {
     lib: {
-      entry: "src/ref.ts",
+      formats: ["es"],
+      entry: "src/index.ts",
       name: "DSState",
       fileName: (format) => `dsstate.${format}.js`,
     },
